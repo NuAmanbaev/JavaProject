325a259290e01f0181a48a465156e922910898a0
isakazy <isakazy@gmail.com>
2023-12-26 19:15:13 -0600

    initial commit

---
+++ src/main/java/HomeWork/HomeWork4.java
@@ -0,0 +1,10 @@
+package HomeWork;
+
+public class HomeWork4 {
+    public static void main(String[] args) {
+        String name = "Nurgazy!" + " ";
+        String  greeting = "Hello" +  " " + "Welcome";
+        System.out.println(name + greeting);
+
+    }
+}
---
+++ src/main/java/HomeWork/Homework5.java
@@ -0,0 +1,60 @@
+package HomeWork;
+
+public class Homework5 {
+    public static void main(String[] args) {
+        byte num1 = 5;
+        byte num2 = 100;
+        byte num3 = 127;
+
+        short short1 = 200;
+        short short2 = 300;
+        short short3 = 1000;
+
+        int int1 = 234234234;
+        int int2 = 12312331;
+        int int3 = 123123121;
+
+        long long1 = 1234123121;
+        long long2 = 454545454;
+        long long3 = 123131231;
+
+        float float1 = 5.5f;
+        float float2 = 6.6f;
+        float float3 = 7.7f;
+
+        double double1 = 14.4;
+        double double2 = 15.5d;
+        double double3 = 16.6d;
+
+        boolean  boolean1 = true;
+        boolean boolean2 = false;
+
+        char char1 = '!';
+        char char2 = '/';
+        char char3 = '?';
+
+        System.out.println(num1 + ","  + num2 + "," + num3);
+        System.out.println(short1 + ","  + short2 + ","  + short3);
+        System.out.println(int1 + ","  + int2 + ","  + int3);
+        System.out.println(long1 + ","  + long2 + ","  + long3);
+        System.out.println(float1 + "," + float2 + ","  + float3);
+        System.out.println(double1 + ","  + double2 + ","  + double3);
+        System.out.println(boolean1);
+        System.out.println(boolean2);
+        System.out.println(char1 + ","  + char2 + ","  + char3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
